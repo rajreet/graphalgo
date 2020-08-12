2e58fe7dad5e7a143645486920cb2cbff8a09a9a
rajreet <rajreetsarkar114@gmail.com>
2020-08-12 12:09:14 +0530

    dfs ver0 completed

--- static/dfs/dfs.js
+++ static/dfs/dfs.js
@@ -2,8 +2,11 @@
 var matrix = [...Array(20)].map(e => Array(20).fill(0));
 var nodes = new Array(20);
 var pos = new Array(20);
+var vis=new Array(20);
+var nodecount =new Array(20);
 nodes.fill(0);
 
+var counter=0;
 //nodes coordinates
 let coods=[
     [50,70],
@@ -41,10 +44,12 @@ function shuffle(array) {
 //add node in svg
 function addNode(val,x,y)
 {
+    d3.select(`#node${val-1}`).remove();
+
     const svg = d3.select('svg');
 
     const g= svg.append('g')
-                .attr('id',"node"+val);
+                .attr('id',`node${val-1}`);
 
     const c = g.append('circle')
                 .attr('cx',x)
@@ -56,7 +61,7 @@ function addNode(val,x,y)
 
     const t =g.append('text')
                 .attr('x',x)
-                .attr('y',y+5)
+                .attr('y',y)
                 .text(val)
                 .attr('fill','#383F51')
                 .attr('alignment-baseline',"central")
@@ -107,13 +112,35 @@ function createEdge(x,y)
         midy1-=25;
     }
     svg.append('path')
+        .attr('id',`edge${x}${y}`)
         .attr('d','M '+x1+' '+y1+' '+'S '+midx1+' '+midy1+' '+x2+' '+y2)
         .attr('stroke','black')
         .attr('fill','none');
 
-    addNode(y+1,x2,y2);
-    addNode(x+1,x1,y1);
+        
+        addNode(y+1,x2,y2);
+    
+        addNode(x+1,x1,y1);
 }
+
+//dfs function
+function DFS(node)
+{
+    //apply transition
+    console.log(node);
+    vis[node]=1;
+    counter++;
+
+    nodecount[node]=counter;
+
+    for(var i=0;i<20;i++)
+    {
+        if(matrix[node][i] && !vis[i])
+            DFS(i);
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded',function(){
     var box=document.querySelector('#dfs');
     box.style.color='white';
@@ -160,7 +187,50 @@ document.addEventListener('DOMContentLoaded',function(){
         return false;
     };
 
+    //run algorithm
     document.querySelector('#run').onclick=function(){
-        console.table(pos);
+        //initialise
+        vis.fill(0);
+        d3.selectAll('g').select('circle')
+                    .style('fill','#F5F2B8')
+        counter=0;
+        
+        //run dfs
+        for(var i=0;i<20;i++)
+        {
+            if(nodes[i] && !vis[i])
+                DFS(i);
+        }
+
+        for(var i=0;i<20;i++)
+        {
+            if(nodes[i])
+            {
+            const c = d3.select(`#node${i}`).select('circle');
+                c.transition()
+                .delay(1000*nodecount[i])
+                .duration(1000)
+                .style('fill','#f35757');
+            }
+        }
+
     };
+
+    //clear graph
+    document.querySelector('#clear').onclick=function(){
+        d3.selectAll('g').remove();
+        d3.selectAll('path').remove();
+
+        for(var i=0;i<20;i++)
+        {
+            nodes[i]=0;
+            for(var j=0;j<20;j++)
+            {
+                matrix[i][j]=0;
+            }
+        }
+
+        document.querySelector('#edgelist').innerHTML="";
+    };
+
 });
